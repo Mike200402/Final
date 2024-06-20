595364e48438d5001a4b76bfc05a7255d8955894
Miguel Flores <dragonex0020@gmail.com>
2024-06-20 11:53:01 -0400

    Creando los componentes iniciales

---
+++ src/componentes/HeaderPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderPage = () => {
+  return (
+    <div>
+        <h1>Home</h1>
+        <h1>About</h1>
+        <h1>Dashboard</h1>
+    </div>
+  )
+}
+
+export default HeaderPage
